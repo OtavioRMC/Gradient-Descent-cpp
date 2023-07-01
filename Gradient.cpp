54115e4d6f6d2bd594f7616b03fe152f5fadb2f7
OtavioRMC <otavioribeiromoreiradacosta@gmail.com>
2023-07-01 15:31:36 -0300

    Update

--- Gradient.cpp
+++ Gradient.cpp
@@ -1,5 +1,8 @@
 #include "Gradient.hpp"
 #include <math.h>
+#include <iostream>
+#include <functional>
+#include <vector>
 
 Gradient::Gradient() {
     m_nDimensions = 0;
@@ -34,3 +37,55 @@ void Gradient::setGradientThreshold(double gradientThreshold){
     m_gradientThreshold = gradientThreshold;
 }
 
+
+bool Gradient::Optimize(std::vector<double> *funcLoc, double *funcVal){
+
+
+    m_currentPoint = m_startPoint;
+    int iterCount =0;
+    double gradientMagnitude = 1.0;
+    while((iterCount < m_maxIterations) && (gradientMagnitude > m_gradientThreshold)){
+        std::vector<double> gradientVector = computeGradientVector();
+        gradientMagnitude = computeGradientMagnitude(gradientVector);
+
+        std::vector<double> newPoint = m_currentPoint;
+        for(int i = 0;i < m_nDimensions;i++){
+            newPoint[i] += -(gradientVector[i] * m_stepSize);
+        }
+
+        m_currentPoint = newPoint;
+        iterCount++;
+    }
+    *funcLoc = m_currentPoint;
+    *funcVal = m_objectFunction(&m_currentPoint);
+    return true;
+}
+
+double Gradient::computeGradient(int dim){
+    std::vector<double> newPoint = m_currentPoint;
+
+    newPoint[dim] += m_gradientStepSize;
+
+    double funcVal1 = m_objectFunction(&m_currentPoint);
+    double funcVal2 = m_objectFunction(&newPoint);
+
+    return (funcVal2 - funcVal1) / m_gradientStepSize; 
+}
+
+std::vector<double> Gradient::computeGradientVector(){
+  
+  std::vector<double> gradientVector = m_currentPoint;
+  for (int i = 0; i < m_nDimensions; i++) {
+    gradientVector[i] = computeGradient(i);
+  }
+  return gradientVector;
+}
+
+double Gradient::computeGradientMagnitude(std::vector<double> gradientVector){
+
+    double gradientMagnitude = 0.0;
+    for (int i = 0; i <  m_nDimensions; i++) {
+        gradientMagnitude += gradientVector[i] * gradientVector[i];
+    }
+    return sqrt(gradientMagnitude);
+}
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "Gradient.cpp"
+#include <iostream>
+#include <functional>
+
+double ObjectFnc(std::vector<double> *funcLoc){
+  double x = funcLoc->at(0);
+  return (x*x);
+}
+
+int main(){
+
+  std::function<double(std::vector<double>*)> p_ObjectFcn{ObjectFnc};
+
+  // Test instance.
+  Gradient solver;
+
+  // Asign the object function.
+   solver.setObjectFn(p_ObjectFcn);
+
+  std::vector<double> startPoint = {-1.0};
+  solver.setStartPoint(startPoint);
+   
+  solver.setStepSize(0.2);
+
+  std::vector<double> funcLoc;
+  double funcVal;
+  solver.Optimize(&funcLoc,&funcVal);
+
+  
+  std::cout << "Function Location(x) : " << funcLoc[0] << std::endl;
+  std::cout << "Function value(y): " << funcVal << std::endl;
+
+  return 0;
+} 
+
